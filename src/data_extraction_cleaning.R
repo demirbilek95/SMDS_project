6a6df54e74493682d8bdee7305d532ab27b03f11
demirbilek95 <dogancandemirbilek@gmail.com>
2020-06-23 15:28:17 +0300

    data merged and some plots are done(not completed)

--- src/data_extraction_cleaning.R
+++ src/data_extraction_cleaning.R
@@ -4,6 +4,10 @@ covid_19_india <- rename(covid_19_india, State = State.UnionTerritory)
 pop_census <- read.csv("data/datasets_557629_1267081_population_india_census2011.csv")
 pop_census <- rename(pop_census, State = State...Union.Territory)
 testing_details <- read.csv("data/datasets_557629_1267081_StatewiseTestingDetails.csv")
+testing_lab <- read.csv("data/ICMRTestingLabs.csv")
+testing_lab <- rename(testing_lab, State = state)
+hospital <- read.csv("data/HospitalBedsIndia.csv")
+hospital <- rename(hospital, State = State.UT)
 
 states <- c("Gujarat","Maharashtra","Madhya Pradesh",
             "Chhattisgarh","Jharkhand","Odisha",
@@ -15,14 +19,39 @@ state_filter <- function(data, states){
   return(filtered_data_set)
 }
 
+select_columns <- function(data, columns){
+  df <- data %>% select(columns)
+  return(df)
+}
+
+
 covid_19_india <- state_filter(covid_19_india, states)
 pop_census <- state_filter(pop_census, states)
 testing_details <- state_filter(testing_details, states)
+testing_lab <- state_filter(testing_lab,states)
+hospital <- state_filter(hospital,states)
 
-covid_19_india <- 
-  covid_19_india %>% 
-  select(Date, State, Confirmed)
+covid_19_india <- select_columns(covid_19_india,c("Date","State","Confirmed"))
+pop_census <- select_columns(pop_census, 
+                             c("State","Population","Rural.population","Urban.population",
+                               "Area","Density","Gender.Ratio"))
+testing_lab <- select_columns(testing_lab, c("lab","State"))
+## Aggregate the number of testing lab
+testing_lab <- testing_lab %>% group_by(State) %>% summarise(num_lab = n())
+
+hospital <- select_columns(hospital,c("State", "TotalPublicHealthFacilities_HMIS"))
+
+# Merging with other files
+temp <- merge(covid_19_india,pop_census,by.x = "State", by.y = "State")
+temp2 <- merge(temp, testing_lab, by.x = "State", by.y = "State" )
+ind_data <- merge(temp2, hospital, by.x = "State", by.y = "State")
 
 write.csv(covid_19_india, "data/covid_19_india_filtered.csv")
 write.csv(pop_census, "data/pop_census_filtered.csv")
 write.csv(testing_details, "data/testing_filtered.csv")
+write.csv(testing_lab, "data/lab_filtered.csv")
+write.csv(hospital, "data/hospital.csv")
+write.csv(hospital, "data/ind_data.csv")
+
+
+
---
+++ src/general_plots.R
@@ -0,0 +1,59 @@
+# Filtering data for states just to try
+
+library(dplyr)
+covid_19_india <- read.csv("data/covid_19_india_filtered.csv")
+covid_19_india
+
+str(covid_19_india)
+
+covid_19_india$State
+
+jharkhand_data  <-  covid_19_india %>% filter(State == "Jharkhand")
+jharkhand_data
+########################################################################
+
+# Plotting total confirmed cases for states
+
+library(ggplot2)
+
+covid_19_india <- read.csv("data/covid_19_india_filtered.csv")
+
+tot_states <- covid_19_india %>% group_by(State) %>% summarise(
+  tot_confirmed = sum(Confirmed)
+)
+
+plot(jharkhand_data$Confirmed)
+
+options(scipen=100)
+ggplot(tot_states,aes(x=State, y = tot_confirmed)) + geom_bar(stat = "identity") + scale_y_continuous("Total Confirmed Cases")
+#ggsave(paste0("plots/total_confirmed_states.png"))
+
+#########################################################################
+
+# Map Plot
+
+library(sp)
+library(RColorBrewer)
+ind1=readRDS("data/IND_adm/IND_adm1.rds")
+
+spplot(ind1, "NAME_1", scales=list(draw=T), colorkey=F, main="India")
+ind1
+
+?merge
+
+ind1$NAME_1 = as.factor(ind1$NAME_1)
+ind1$fake.data = merge(ind1@data,tot_states,by.x = "NAME_1", by.y = "State",all.x = TRUE)$tot_confirmed
+
+
+spplot(ind1,"fake.data",main = "Central India and Total Confirmed Cases",
+       colorkey=T, scales=list(draw=T))
+
+ind1$fake.data[is.na(ind1$fake.data)] <- 0
+
+?spplot
+
+
+merge(ind1@data,tot_states,by.x = "NAME_1", by.y = "State",all.x = TRUE)
+
+
+
