165227b5a448d862d6bccb167e18bd3d1705c6a9
wilsongefferson <pierpa.m.96live.it>
2020-06-23 11:11:14 +0200

    An error was detected ("Chhattisgarh" was empty), now should be ok

--- src/re_arrange_covid_india_dataset.R
+++ src/re_arrange_covid_india_dataset.R
@@ -2,27 +2,36 @@
 
 library(tidyr)
 
-# inport covid_india_19_filtered
+# import covid_india_19_filtered
 india_19 <- read.csv('~/Documents/Statistical_method_data_science/smds_exercises/SMDS_project/data/covid_19_india_filtered.csv', row.names=1)
 
 states <- as.list(levels(india_19$State)) # take the states
 states_list <- vector("list", length(states)) # prepare a list of dataframes
 
 for(state in states){ # for each state, build a dataframe composed by the days and confirmed cases
-  Date <- as.character.Date(india_19$Date[india_19$State == state]) # take days
+  Date <- india_19$Date[india_19$State == state] # take days
   confirmed <- india_19$Confirmed[india_19$State == state] # take confirmed cases
   
   region <- data.frame(Date, confirmed) # build dataframe
+  region$Date <- as.Date(region$Date, format = "%d/%m/%Y") # conversion to EU format
   colnames(region)[2] <- state # rename the "confirmed" field with the name of the state
   
   states_list[[state]] <- region # put the dataframe into the list
 }
 
-merged <- Reduce(function(...) merge(..., all = TRUE), states_list) # make an outerjoint according date fields
-merged <- merged[order(as.Date(merged$Date, format="%d/%m/%Y")),] # order by date
+merged <- merge(states_list[["Chhattisgarh"]], states_list[["Gujarat"]], by = "Date", all = TRUE)
+merged <- merge(merged, states_list[["Jharkhand"]], by = "Date", all = TRUE)
+merged <- merge(merged, states_list[["Madhya Pradesh"]], by = "Date", all = TRUE)
+merged <- merge(merged, states_list[["Maharashtra"]], by = "Date", all = TRUE)
+merged <- merge(merged, states_list[["Odisha"]], by = "Date", all = TRUE)
+merged <- merge(merged, states_list[["West Bengal"]], by = "Date", all = TRUE)
+
+# make an outerjoint according date fields (ERROR: "Chhattisgarh" is empty... consider the above code)
+# merged <- Reduce(function(x, y, ...) merge(x, y, all = TRUE, sort = TRUE, ...), states_list)
+
+merged[is.na(merged)] <- 0 # set to zero all NA values
 
 write.csv(merged, "data/re_arranged_covid_19_india_filtered.csv") # export it
 
 # at the end we have a dataframe composed by dates and, for each date,
 # the corrisponding confirmed cases state by state.
-
