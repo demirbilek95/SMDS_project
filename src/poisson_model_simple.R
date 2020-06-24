7cfaf5ddec77d893d88a193fa7d66b4db4ee0aa7
wilsongefferson <pierpa.m.96live.it>
2020-06-24 23:28:07 +0200

    problem on ggplot

--- src/poisson_model_simple.R
+++ src/poisson_model_simple.R
@@ -2,49 +2,87 @@
 
 library(tidyr)
 library(MASS) 
+library(dplyr)
 
 # load datasets
 covid_19_india <- read.csv('~/Documents/Statistical_method_data_science/smds_exercises/SMDS_project/data/re_arranged_covid_19_india_daily_infected_filtered.csv', row.names = 1)
 testing <- read.csv('~/Documents/Statistical_method_data_science/smds_exercises/SMDS_project/data/testing_filtered.csv', row.names = 1)
 
-# solve conflict lengths among responce and covariates: confirmed cases start
-# one mounth before store the negative and positive in testing dataset (Maharashtra)
-start <- length(covid_19_india$Maharashtra) - 
-  length(testing$Negative[testing$State == "Maharashtra"])
-
-zeros <- rep(0, start) # used to fill the first negative and positive with zeros
-negative <- testing$Negative[testing$State == "Maharashtra"]
-for (v in zeros){negative <- c(v, negative)}
-
-positive <- testing$Positive[testing$State == "Maharashtra"]
-for (v in zeros){positive <- c(v, positive)}
+prediction_period <- 7 # one week of prediction
+end <- length(covid_19_india$Maharashtra) # number of days
 
 # define train and test sets
-set.seed(123)
-smp_size <- floor(0.75 * length(covid_19_india$Maharashtra)) # 75% of the sample size
-train_ind <- sample(seq_len(length(covid_19_india$Maharashtra)), size = smp_size)
+train_ind <- end-prediction_period
 
 # responce partition
-train_y <- covid_19_india$Maharashtra[train_ind]
-test_y <- covid_19_india$Maharashtra[-train_ind]
+train_confirmed <- covid_19_india$Maharashtra[0:train_ind]
+test_confirmed <- covid_19_india$Maharashtra[(train_ind+1):end]
 
 # covariates partition
-train_x1 <- negative[train_ind]
-test_x1 <- negative[-train_ind]
+train_negative <- testing$Negative[0:train_ind]
+test_negative <- testing$Negative[(train_ind+1):end]
 
-train_x2 <- positive[train_ind]
-test_x2 <- positive[-train_ind]
+train_positive <- testing$Positive[0:train_ind]
+test_positive <- testing$Positive[(train_ind+1):end]
+
+train_tot_sample <- testing$TotalSamples[0:train_ind]
+test_tot_sample <- testing$TotalSamples[(train_ind+1):end]
+
+# prepare train dataset
+train_data = data.frame(date = 1:train_ind, confirmed = train_confirmed, 
+                  yest_confirmed = c(0, covid_19_india$Maharashtra[0:(train_ind-1)]),
+                  tot_sample = train_tot_sample, negative = train_negative, positive = train_positive)
 
 # make the poisson model
-data = data.frame(y = train_y, negative = train_x1, positive = train_x2)
-poisson_model <- glm(data = data, y ~ negative + positive, family = poisson(link = "log"))
+poisson_model <- glm(data = train_data, confirmed ~ 
+                       yest_confirmed + tot_sample + positive, 
+                     family = poisson(link = "log"))
+
 summary(poisson_model)
 
-# make prediction
-newdata = data.frame(negative = test_x1, positive = test_x2)
-predict(poisson_model, newdata = newdata)
+# prepare test dataset
+test_data = data.frame(date = (train_ind+1):end, 
+                       yest_confirmed = c(covid_19_india$Maharashtra[train_ind], rep(0, prediction_period-1)),
+                       tot_sample = test_tot_sample, negative = test_negative, positive = test_positive)
 
-# compute informatice plot
+# make prediction
+for(i in 2:prediction_period){
+  # reinsert the predicted value into the model to predict the next one
+  predicted_value <- predict(poisson_model, newdata = test_data[i-1, ])
+  test_data$yest_confirmed[i] <- predicted_value
+}
+
+# visual comparison between real confirmed and predictated
+test_data$yest_confirmed
+test_confirmed
+
+# make plot
+pred_train <- predict(poisson_model, newdata = train_data)
+plot(train_confirmed)
+lines(pred_train,col="red")
+
+pred_test <- predict(poisson_model, newdata = test_data)
+options("scipen"=100, "digits"=4)
+test_confirmed
+pred_test
+plot(test_confirmed)
+lines(pred_test,col="red")
+
+mean((test_confirmed - pred_test)^2/(nrow(test_data)))
+mad((test_confirmed - pred_test)/(nrow(test_data)))
+
+data <- bind_rows(train_data, test_data)
+select(data, date, yest_confirmed, tot_sample, positive)
+
+pred <- predict(poisson_model, newdata = select(data, date, yest_confirmed, tot_sample, positive))
+
+library(ggplot2)
+
+ggplot(data = covid_19_india, aes(x = date, y = Maharashtra)) + 
+  geom_line(col = 'blue') + 
+  geom_line(aes(y = pred), col = 'red')
+
+# compute informative plot
 library(jtools)
 library(ggstance)
 library(broom)
