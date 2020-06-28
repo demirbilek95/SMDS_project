3e8dd4f4b76a7b712f40ee5dba0e03bfa6cc42aa
wilsongefferson <pierpa.m.96live.it>
2020-06-28 15:50:44 +0200

    plots adjusted with ggplot in model.R

--- src/model.R
+++ src/model.R
@@ -1,6 +1,7 @@
 library(tidyverse) # data manipulatinon
 library(lubridate) # for date formatting
 library(ggplot2) # for plotting
+library(ggpubr) # to make plots side-byside
 
 # Preparing Data
 
@@ -53,9 +54,16 @@ model <- function(state, glm_model_family, link_func) {
   df$daily[df$daily < 0] <- 0
   
   # plot what we may try to forecast
-  par(mfrow=c(1,2))
-  plot(df$daily)
-  plot(df$Confirmed)
+  p1 <- ggplot(df, aes(x = 1:length(daily), y = daily)) + geom_point() +
+        labs(x = "days", y = "cases", subtitle = "daily")
+  
+  p2 <- ggplot(df, aes(x = 1:length(Confirmed), y = Confirmed)) + geom_point() +
+    labs(x = "days", y = "cases", subtitle = "cumulative")
+  
+  parr <- ggarrange(p1, p2, ncol = 2, nrow = 1)
+  p <- annotate_figure(parr, top = text_grob(paste("Confirmed cases in", state)))
+  
+  print(p)
   
   # train-test split, last 1 week as a test set can be 10 days or 14 days
   train_ind <- nrow(df) - 7 
@@ -87,11 +95,14 @@ model <- function(state, glm_model_family, link_func) {
   
   # comparing predictions and real values
   cat("predictions:",pred)
-  cat("\nreal values",test$Confirmed)
+  cat("\nreal values:",test$Confirmed)
   
   # just plotting test set and predictins together
-  plot(test$Confirmed)
-  lines(pred,col="red")
+  p <- ggplot(test, aes(x = 1:length(Confirmed), y = Confirmed)) + geom_point() +
+    geom_line(aes(y = pred), color = "red") + 
+    labs(x = "counter", y = "cases", title = "visual check on predicted range")
+  
+  print(p)
   
   # MSE and MAD check
   mean((test$Confirmed - pred)^2/(nrow(test)))
@@ -103,10 +114,17 @@ model <- function(state, glm_model_family, link_func) {
   # red is model, blue is observations
   
   obs_pred <- append(train$Confirmed,pred)
-  ggplot(data=df,aes(x=num_day,y=Confirmed)) + geom_line(col='blue') + geom_line(aes(y=obs_pred),col='red')
+  
+  real_pred_data <- data.frame(days = df$num_day, 
+                               real_confirmed = df$Confirmed, 
+                               pred_confirmed = obs_pred)
+  
+  ggplot(data=real_pred_data, aes(x=num_day, y=Confirmed)) + 
+    geom_line(col='blue') + 
+    geom_line(aes(y=obs_pred), col='red') + 
+    labs(title = "Prediction over cumulative cases")
   
 }
 
 model("Maharashtra",gaussian,"identity")
-
 model("Jharkhand", gaussian, "identity")
